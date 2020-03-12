1d1332fe42475dbfcabbc21b6b538ed539003cf7
Artur Ponomarenko <arthur.ponomar@gmail.com>
2020-03-13 00:05:11 +0200

    Fix building 2D graphs.

--- GraphBuilderDiplomaWork/Recognition/Recognizers/FakeRecognizer.swift
+++ GraphBuilderDiplomaWork/Recognition/Recognizers/FakeRecognizer.swift
@@ -13,6 +13,7 @@ class FakeRecognizer: EquationRecognizer {
     func recognize(_ image: UIImage, completion: @escaping ((Result<[Equation], Error>) -> Void)) {
         let functions: [Any] = [
         { (x: Float) -> Float in pow(x, 2) },
+        { (x: Float) -> Float in sqrt(pow(x, 2)-pow(x, 4)) },
         { (x: Float, y: Float) -> Float in pow(x, 2) + pow(y, 2) },
         { (x: Float, y: Float) -> Float in pow(x, 3) + pow(y, 3) },
         { (x: Float, y: Float) -> Float in sin(10 * (x * x + y * y))/10 },
@@ -22,11 +23,12 @@ class FakeRecognizer: EquationRecognizer {
         
         completion(Result.success([
             Equation(latex: "x^2", function: functions[0]),
-            Equation(latex: "x^2 + y^2", function: functions[1]),
-            Equation(latex: "x^3 + y^3", function: functions[2]),
-            Equation(latex: "sin(10(x^2+y^2))/10", function: functions[3]),
-            Equation(latex: "sin(5x)*cos(5y)/5", function: functions[4]),
-            Equation(latex: "(x^2+y^2)^0.5", function: functions[5]),
+            Equation(latex: "sqrt(x^2-x^4)", function: functions[1]),
+            Equation(latex: "x^2 + y^2", function: functions[2]),
+            Equation(latex: "x^3 + y^3", function: functions[3]),
+            Equation(latex: "sin(10(x^2+y^2))/10", function: functions[4]),
+            Equation(latex: "sin(5x)*cos(5y)/5", function: functions[5]),
+            Equation(latex: "(x^2+y^2)^0.5", function: functions[6]),
         ]))
     }
     
